19d8a0002db07dd0f2b106c59d4bc024ddac8eb9
Deray <deraygo@gmail.com>
2016-03-23 14:28:49 +0800

    cache updated result, no need to query every time.

--- js/popup.js
+++ js/popup.js
@@ -24,6 +24,7 @@ function updateFundDiv(fundDiv, title, raw) {
   var diff = $('.Fl-start.number.Pt-8', raw);
 
   var update = $('.Ta-end.Mt-16.remark', raw).text();
+  update = update.replace('更新', '');
   $('.update', fundDiv).text(update);
 
   var symbol = ''
@@ -52,6 +53,30 @@ function restore_options(callback) {
   }, callback);
 }
 
+function restore_fund(key, callback) {
+  var json = {};
+  json[key] = {};
+  chrome.storage.sync.get(json, callback);
+}
+
+function getYesterday() {
+  var now = new Date();
+  var day = now.getDay();
+
+  var num = 1;
+  if (day == 0)
+    num = 2;
+  else if (day == 1)
+    num = 3;
+
+  var yesterday = new Date(now.getTime() - 1000 * 60 * 60 * 24 * num);
+  var yyyy = yesterday.getFullYear();
+  var mm = yesterday.getMonth() + 1;
+  var dd = yesterday.getDate();
+
+  return yyyy + '/' + mm + '/' + dd;
+}
+
 function loadFund(link, fund){
   var fundDiv = createFundDiv();
   $(fundDiv).hide();
@@ -65,24 +90,51 @@ function loadFund(link, fund){
     openTab(link + fund.key);
   });
 
-  $.ajax({
-    url: link + fund.key,
-    type: 'GET',
-    success: function(data) {
-      var i = data.indexOf('<ul class="Grid Pb-2"');
-      var j = data.indexOf('</ul>', i) + 5;
-      var raw = data.substring(i, j);
+  //--------------
+  restore_fund(fund.key, function(items) {
+    var cache = items[fund.key];
+    var yesterday = getYesterday();
 
-      var i = data.indexOf('<li class="Grid-U-3-4">');
-      var j = data.indexOf('</li>', i) + 5;
-      var title = data.substring(i, j);
-
-      updateFundDiv(fundDiv, title, raw);
-    },
-    /*error: function(data) {
-        console.log(data);
-    }*/
+    if (cache[yesterday]){
+      loadByCache(cache.title, cache[yesterday]);
+    } else {
+      loadByAJAX(cache, yesterday);
+    }
   });
+
+  //--------------
+  function loadByCache(title, raw) {
+    updateFundDiv(fundDiv, title, raw);
+  }
+  //--------------
+  function loadByAJAX(cache, yesterday) {
+    $.ajax({
+      url: link + fund.key,
+      type: 'GET',
+      success: function(data) {
+        var i = data.indexOf('<ul class="Grid Pb-2"');
+        var j = data.indexOf('</ul>', i) + 5;
+        var raw = data.substring(i, j);
+
+        var i = data.indexOf('<li class="Grid-U-3-4">');
+        var j = data.indexOf('</li>', i) + 5;
+        var title = data.substring(i, j);
+
+        updateFundDiv(fundDiv, title, raw);
+
+        //save
+        cache.title = title;
+        cache[yesterday] = raw;
+        var json = {};
+        json[fund.key] = cache;
+        chrome.storage.sync.set(json, function() {
+        });
+      },
+      /*error: function(data) {
+          console.log(data);
+      }*/
+    });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
